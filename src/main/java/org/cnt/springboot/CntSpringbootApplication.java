ce922dc4557c7436aca11cd63e989f446fb4450c
coding-new-talking <1511937685@qq.com>
2019-03-14 09:32:19 +0800

    init

---
+++ src/main/java/org/cnt/springboot/CntSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.cnt.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CntSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CntSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/cnt/springboot/controller/RedisController.java
@@ -0,0 +1,27 @@
+package org.cnt.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lixinjie
+ * @since 2019-03-13
+ */
+@RequestMapping("/redis")
+@RestController
+public class RedisController {
+
+	@Autowired
+	private RedisTemplate<Object, Object> redisTemplate;
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	
+	@GetMapping("/show")
+	public String show() {
+		return redisTemplate + "|" + stringRedisTemplate;
+	}
+}
